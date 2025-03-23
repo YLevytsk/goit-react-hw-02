5c81fb8ca31adc129619aa1f36e11569ffaf16ad
Yuliia <lupin773311asd@gmail.com>
2025-03-23 18:34:27 +0000

    xcds

--- src/components/Description/Description.jsx
+++ src/components/Description/Description.jsx
@@ -4,9 +4,11 @@ import styles from './Description.module.css';
 export default function Description() {
   return (
     <div className={styles.description}>
-      <h1 className={styles.title}>Café Sip Happens</h1>
+      <h1 className={styles.title}>Sip Happens Café
+</h1>
       <p className={styles.text}>
         Please leave your feedback about our service by selecting one of the options below.
+
       </p>
     </div>
   );
